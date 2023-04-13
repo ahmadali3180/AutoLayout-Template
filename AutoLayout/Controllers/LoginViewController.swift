cd4f5e3296d0c59ac35db4c6f50c28d1c220e30c
mahmadali3180 <mahmadali3180@gmail.com>
2023-04-14 02:12:44 +0500

    Design Changes

--- AutoLayout/Controllers/LoginViewController.swift
+++ AutoLayout/Controllers/LoginViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  AutoLayout
 //
-//  Created by M. Ahmad Ali on 13/04/2023.
+//  Created by M. Ahmad Ali on 14/04/2023.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
-
